2d19df40bb5fb5f8f670e336cc68b07b5e9818a2
Roman Vildanov <vildanovv.rr@gmail.com>
2022-11-13 21:12:53 +0200

    add searh

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,15 +1,33 @@
 import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
 
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const heandleChange = e => {
+    setSearchQuery(e.target.value);
+  };
+
+  const heandelSubmit = e => {
+    e.preventDefault();
+    onSubmit(searchQuery);
+  };
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={heandelSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
-          Select a region and press enter
+      <Select
+        defaultValue={'DEFAULT'}
+        aria-label="select"
+        name="region"
+        required
+        onChange={heandleChange}
+      >
+        <option value="DEFAULT" disabled>
+          Choose a salutation ...
         </option>
         <option value="africa">Africa</option>
         <option value="america">America</option>
@@ -17,6 +35,7 @@ export const SearchForm = () => {
         <option value="europe">Europe</option>
         <option value="Oceania">Oceania</option>
       </Select>
+      1``
     </SearchFormStyled>
   );
 };
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,35 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useState, useEffect } from 'react';
+import { fetchByRegion } from '../service/country-service';
 
 export const CountrySearch = () => {
+  const [query, setQuery] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+
+  const onHeandelSubmit = query => {
+    setQuery(query);
+  };
+
+  useEffect(() => {
+    const getRegion = async () => {
+      try {
+        const data = await fetchByRegion(query);
+        setCountries(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    getRegion();
+  }, [query]);
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={onHeandelSubmit} />
+        {countries && <CountryList countries={countries} />}
       </Container>
     </Section>
   );
